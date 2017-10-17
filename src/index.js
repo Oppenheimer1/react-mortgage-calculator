ebda0174eeb436c78b5e2268c30704ede7f0daee
LARS BERG <larsgbr@gmail.com>
2017-10-17 13:51:42 -0500

    added additional functionality, updated replacement from React.createClass to createReactClass from requiring create-react-class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+var createReactClass = require('create-react-class');
 
 
 var calculcatePayment = function(principal, years, rate){
@@ -20,15 +21,74 @@ var calculcatePayment = function(principal, years, rate){
 	}
 
 	return {monthlyPayment: monthlyPayment};
-}
+};
 
 
-var Header = React.createClass({
-	render: function(){
+var Header = createReactClass({
+	render: function () {
 		return(
 			<header>
 				<h1>{this.props.title}</h1>
 			</header>
+		);
+	}
+});
+
+var MortgageCalculator = createReactClass({
+	getInitialState: function(){
+		return{
+			principal: this.props.principal,
+			years: this.props.years,
+			rate: this.props.rate
+		};
+	},
+	principalChange: function(event){
+		this.setState({principal: event.target.value});
+	},
+	yearsChange: function(event){
+		this.setState({years: event.target.value});
+	},
+	rateChange: function(event){
+		this.setState({rate: event.target.value});
+	},
+	render: function(){
+		var payment = calculcatePayment(this.state.principal, this.state.years, this.state.rate);
+		var monthlyPayment = payment.monthlyPayment;
+
+		return(
+			<div className="content">
+				<div className="form">
+					<div>
+						<label>Principal:</label>
+						<input type="text" value={this.state.principal} onChange={this.principalChange}/>
+					</div>
+					<div>
+						<label>Years:</label>
+						<input type="text" value={this.state.years} onChange={this.yearsChange}/>
+					</div>
+
+					<div>
+						<label htmlFor="rate">Rate:</label>
+						<input type="text" value={this.state.rate} onChange={this.rateChange}/>
+					</div>
+				</div>
+				<h2>Monthly Payment: <span className="currency">{Number(monthlyPayment.toFixed(2)).toLocaleString()}</span></h2>
+
+			</div>
+
+			);
+	}
+
+});
+
+
+var App = createReactClass({
+	render: function(){
+		return(
+			<div>
+				<Header title = "Mortgage Calculator built using React" />
+				<MortgageCalculator principal = "200000" years="30" rate="5" />
+			</div>
 		);
 	}
 });
